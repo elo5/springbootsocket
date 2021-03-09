612592fdbd86e799dc5928b1593738f4cfc7bb7c
elo <elo5@qq.com>
2021-03-09 09:40:20 +0800

    it works

---
+++ src/main/java/com/lilwork/springbootsocket/SpringbootsocketApplication.java
@@ -0,0 +1,13 @@
+package com.lilwork.springbootsocket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootsocketApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootsocketApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/lilwork/springbootsocket/SpringbootsocketApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lilwork.springbootsocket;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootsocketApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
